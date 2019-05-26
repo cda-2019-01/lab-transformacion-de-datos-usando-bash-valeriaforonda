c4075b45d4eec54dcc90973c2c25abfff918cd5d
segiraldoma <50236398+segiraldoma@users.noreply.github.com>
2019-05-26 15:45:28 -0500

    final

--- script.sh
+++ script.sh
@@ -7,14 +7,14 @@ done
 ### letras mayúsculas que son la primera "columna" ya que está va siempre y no itera
 ### dentro del ciclo for. Se imprime el nombre del archivo, el contador, esa letra mayúscula
 ### y el valor que va iterando en el resto de filas.
-for a in $(seq 1 3)
-do  cont=0
-    for row in $(cat dataN${a}.csv)
+for a in $(seq 1 3) 
+do  cont=0 
+    for row in $(cat dataN${a}.csv) 
     do cont=$((cont+1))
         fila=($( echo ${row[*]} | sed 's/,/\t/g'))
         for x in $( echo ${fila[*]} | sed 's/[A-Z]//' )
-        do  echo data${a}.csv','$cont','${fila[0]}','$x >> out.3
-        done
+        do  echo data${a}.csv','$cont','${fila[0]}','$x >> out.3 
+        done 
     done
 done
 cat out.3
